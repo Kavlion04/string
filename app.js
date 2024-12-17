a6e1dd11c966489d4a43da74f6f18d1ca467c33c
aqqullo <xaqqulloabdusamotov@gmail.com>
2024-12-17 21:00:19 +0500

    fgn

---
+++ app.js
@@ -0,0 +1,58 @@
+//1 masala
+
+
+
+// 2 masala
+
+// function app(arr) {  
+//     let arr2 = arr.reverse();
+//     for (let i = 0; i < arr2.length; i++) {
+//         console.log(arr2[i]);
+//     }
+//     return arr2
+// }
+// let arr = ["sardor" , "Aziz" , "jalol" , "qosim"];
+// console.log(app(arr))
+
+
+//3 masala 
+
+// function soz(gap) {
+//     let sozlar = gap.split(" ");
+//     let soz = sozlar.map((soz) => {
+//         return soz.split("").reverse().join(""); 
+//     });   
+
+//     return soz.join(" ");
+// }
+// let arr = "Muhammadrizo Front-end Developer";
+// let res = soz(arr);
+// console.log(res);
+
+
+// 4 masala
+// function uzunlik(gap) {
+//     let sozlar = gap.trim().split(" ");
+//     return sozlar[sozlar.length - 1].length;
+// }
+// let gap2 = "Mening akam 1999 yilda tug'ilgan";
+// console.log(uzunlik(gap2) , "ta harf bor");
+
+
+
+// 6 masala
+// function app (arrs) {
+//     return arrs.split(" ").join("");
+    
+// }
+// console.log(app("Hello World! How are you?"));
+
+//7 masala 
+// function sum(str) {
+//     return [...str].reduce((sum, char) => sum + (Number(char) || 0), 0);
+//   }
+// console.log(sum("abc1123def45") , ": yig'indi");
+  
+
+
+
